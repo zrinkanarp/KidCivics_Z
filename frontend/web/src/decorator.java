2544ce22e5d94e6d031fcbc270331eff78abbf46
zrinkanarp <zrinkanarp@users.noreply.github.com>
2025-11-15 17:10:58 +0000

    Add decorator.java, demo-model-util.java

---
+++ frontend/web/src/decorator.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/kidcivics_z/frontend/web/src/decorator.java
+// Generated at: 2025-11-27T15:10:54.723Z
+
---
+++ frontend/web/src/demo-model-util.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/kidcivics_z/frontend/web/src/demo-model-util.java
+// Generated at: 2025-12-03T23:45:46.575Z
+
